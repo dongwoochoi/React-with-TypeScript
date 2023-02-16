7b612940b44d34aa3ada7a0813649464b30e2def
dongwoochoi <dong99woo@gmail.com>
2023-02-16 23:55:34 +0900

    end of part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { useState } from 'react';
+import { useState,Dispatch, SetStateAction } from 'react';
 
 function App() {
 
@@ -8,6 +8,8 @@ function App() {
   let [like, set_like] = useState<number[]>([0, 0, 0]);
   let [modal, set_modal] = useState<boolean>(false);
   let [which, set_which] = useState<number>(0);
+  let [input, set_input] = useState<string>("");
+  
   return (
     <div className="App">
       <div className='black-nav'>
@@ -31,26 +33,35 @@ function App() {
                   set_like(copy)  
                   } }>👍</span>
                 { like[i] }
+                <button onClick={()=> { 
+                  let re = [...title];
+                  re.shift();
+                  set_title(re);
+                 }}>글 삭제</button>
               </h4>
               <p>2월 17일 발행</p>
             </div>
           )
         })
       }
+      <input onChange={(e)=> { set_input(e.target.value) }}/>
+      <button onClick={()=>{ set_title([input,...title]) }}>발행</button>
       {
-          modal == true ? <Modal props={title} which={which}></Modal> : null
+          modal == true ? <Modal props={title} which={which} set_title={ set_title }></Modal> : null
       }
     </div>
   );
 }
 
-const Modal = ({props, which} : {props:Array<string>; which:number;}) => {
+
+const Modal = ({props, which, set_title} : {props:Array<string>; which:number; set_title:React.Dispatch<React.SetStateAction<string[]>>;}) => {
   return (
     <div className="modal">
       <h4>제목</h4>
       <div>{props[which]}</div>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={ ()=>{ set_title(['여자 코트 추천', '파이썬 강의 추천', '타입스크립트 강의 추천']) } }>글제목 변경</button>
     </div>
   )
 }
